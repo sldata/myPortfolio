f3884eee5379b98b1f50e2266566194c438b3515
Sławomir <35075574+sldata@users.noreply.github.com>
2022-10-09 22:18:14 +0200

    stylized mobile navigation and add script to hide primary main navigation in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,38 @@
 const burgerBtn = document.querySelector('.burger-btn')
+const mainNav = document.querySelector('.main-nav')
 const nav = document.querySelector('.nav')
+const navItems = document.querySelectorAll('.nav__item')
+const navItemsAnimation = document.querySelector('.nav-items-animation') 
 
 
 const showNavigation = () =>{
     nav.classList.toggle('nav--active')
+   
+    handleNavItemsAnimation()
+    hideMainNavigation()
 }
+
+const hideMainNavigation =() =>{
+if(nav.classList.contains('nav--active')){
+    mainNav.style.display = "none"
+} else{
+    mainNav.style.display = "block"
+}
+}
+
+const handleNavItemsAnimation = () =>{
+    let delayTime = 0
+    navItems.forEach((item) =>{
+        item.classList.toggle ('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's'
+        delayTime++
+    })
+    
+
+
+    
+}
+
+
 
 burgerBtn.addEventListener('click', showNavigation)
