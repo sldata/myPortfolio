e332e60997c485cae698e1351fabbc4cbe879606
Sławomir <35075574+sldata@users.noreply.github.com>
2022-10-11 23:32:01 +0200

    created and stylized responsive navigation

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ const burgerBtn = document.querySelector('.burger-btn')
 const mainNav = document.querySelector('.main-nav')
 const nav = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__item')
-const navItemsAnimation = document.querySelector('.nav-items-animation') 
+const navItemsAnimation = document.querySelector('.nav-items-animation')
 
 
 const showNavigation = () =>{
     nav.classList.toggle('nav--active')
     handleNavItemsAnimation()
-  
+    
 }
 
 
