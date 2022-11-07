aed9dc27d2389acd234586f97360ba0ac2158903
Sławomir <35075574+sldata@users.noreply.github.com>
2022-11-07 23:35:21 +0100

    added responsivness to about section, added closing function of navigation while clicking on link

--- src/js/main.js
+++ src/js/main.js
@@ -4,24 +4,27 @@ const nav = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__item')
 const navItemsAnimation = document.querySelector('.nav-items-animation')
 
-
-const showNavigation = () =>{
-    nav.classList.toggle('nav--active')
-    handleNavItemsAnimation()
-    
+const showNavigation = () => {
+	nav.classList.toggle('nav--active')
+	handleNavItemsAnimation()
+    closeNavigation()
 }
 
-
-const handleNavItemsAnimation = () =>{
-    let delayTime = 0
-    navItems.forEach((item) =>{
-        item.classList.toggle ('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's'
-        delayTime++
-    })
-      
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+	navItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
 }
 
-
+const closeNavigation = () => {
+	navItems.forEach(el =>
+		el.addEventListener('click', function (e) {
+			nav.classList.remove('nav--active')
+		})
+	)
+}
 
 burgerBtn.addEventListener('click', showNavigation)
